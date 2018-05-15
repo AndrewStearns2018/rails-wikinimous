5ac5e0e5b1da2d76f4148dc19a81165ed12bc1b2
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-15 17:02:41 +0200

    need only to add delete

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+   before_action :set_article, only: [:show, :edit, :update, :destoy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    redirect_to articles_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to articles_path
+  end
+
+  def detroy
+    @article.detroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
